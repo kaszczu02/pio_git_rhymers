b70acdfb4c303ae6a939fb3477f8c685d8beadd4
Olek Golus <olekgolus02@gmail.com>
2023-04-23 16:39:49 +0200

    Extract constants, add methods overrides, rename variables to be more specific

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -2,21 +2,21 @@ package edu.kis.vh.nursery.list;
 
 public class IntLinkedList {
 
-    Node last;
-    int i;
+    public static final int EMPTY_LIST_ERROR = -1;
+    public Node lastNode;
 
     public void push(int i) {
-        if (last == null)
-            last = new Node(i);
+        if (isEmpty())
+            lastNode = new Node(i);
         else {
-            last.next = new Node(i);
-            last.next.prev = last;
-            last = last.next;
+            lastNode.nextNode = new Node(i);
+            lastNode.nextNode.prevNode = lastNode;
+            lastNode = lastNode.nextNode;
         }
     }
 
     public boolean isEmpty() {
-        return last == null;
+        return lastNode == null;
     }
 
     public boolean isFull() {
@@ -25,15 +25,15 @@ public class IntLinkedList {
 
     public int top() {
         if (isEmpty())
-            return -1;
-        return last.value;
+            return EMPTY_LIST_ERROR;
+        return lastNode.value;
     }
 
     public int pop() {
         if (isEmpty())
-            return -1;
-        int ret = last.value;
-        last = last.prev;
+            return EMPTY_LIST_ERROR;
+        int ret = lastNode.value;
+        lastNode = lastNode.prevNode;
         return ret;
     }
 
