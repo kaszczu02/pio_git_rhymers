6abc2f20a4228b9a9f3cdc095b0e43996c689276
kaszczu02 <240701@edu.p.lodz.pl>
2023-05-29 15:09:54 +0200

    17.Wygenerowanie szkieletu dokumentacji klasy FIFORhymer

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,9 +1,17 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Klasa `FIFORhymer` rozszerza klasę `DefaultCountingOutRhymer` i implementuje zachowanie pierwszy na wejściu, pierwszy na wyjściu (FIFO) dla liczenia elementów.
+ */
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
     private final DefaultCountingOutRhymer rhymer = new DefaultCountingOutRhymer();
 
+    /**
+     *Pobiera i usuwa najwyższy element ze stosu rhymer.
+     *Przesłania metodę `countOut()` z klasy `DefaultCountingOutRhymer`.
+     * @return wartość najwyższego elementu na stosie rhymer
+     */
     @Override
     public int countOut() {
         while (!callCheck())
